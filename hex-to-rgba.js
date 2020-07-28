f5f0109bddd5b2b9f81b0eb1f9d139273f2c3912
josuel-alves <josuel.alves1@gmail.com>
2020-07-27 21:11:21 -0300

    Optimized log

--- hex-to-rgba.js
+++ hex-to-rgba.js
@@ -1,14 +1,14 @@
 
 function hexToRgb(hex, alpha)
 {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex); 
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     var value = result ? {
-        r: parseInt(result[1], 16), 
-        g: parseInt(result[2], 16), 
-        b: parseInt(result[3], 16) 
-    } : null; 
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    }:null;
 
-    retorno = 'rgba('+ value.r +','+ value.g +','+ value.b +','+ alpha +')'; 
-    return retorno; 
+    retorno = 'rgba('+ value.r +','+ value.g +','+ value.b +','+ alpha +')';
+    return retorno;
 }
-console.log(hexToRgb('#0099ff','.5'));
+//console.log(hexToRgb('#0099ff','.5'));
